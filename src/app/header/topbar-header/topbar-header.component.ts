8a551468a5ce2c7c705bdbec8dec02271fe0910a
arastooghodsi <arastoo.ghodsi.ai@gmail.com>
2020-11-22 23:48:08 +0330

    change a few header

--- src/app/header/topbar-header/topbar-header.component.ts
+++ src/app/header/topbar-header/topbar-header.component.ts
@@ -3,7 +3,7 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-topbar-header',
   templateUrl: './topbar-header.component.html',
-  styleUrls: ['./topbar-header.component.sass']
+  styleUrls: ['./topbar-header.component.css']
 })
 export class TopbarHeaderComponent implements OnInit {
 
