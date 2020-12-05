76f13f39143169e99a2d74cec2fe5904b7e8b004
arastooghodsi <arastoo.ghodsi.ai@gmail.com>
2020-12-06 00:18:15 +0330

    create reactive form

--- src/app/Havadar/login-page/login-page.component.ts
+++ src/app/Havadar/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  inputForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.inputForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      pass: new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.inputForm);
   }
 
 }
